9fe812b7e039b05dfddba60eef567a5481e8ac99
Karl-Haupt <karlhaupt1@gmail.com>
2021-11-10 13:12:14 +0200

    User routes & Controller methods

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,16 @@ const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
 
+const errorMiddleware = require('./middleware/errors');
+
 dotenv.config({ path: 'backend/config/config.env' });
 
 app.use(express.json());
 
 //TODO: Set up for image upload
 //TODO: Import all routes
+app.use('/api/v1', auth);
+
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,30 @@
-//login
+const User = require('../models/user');
+
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError = require('../middlewares/catchAsyncError');
+//TODO: sendToken, sendEmail
+
+//Login User => /api/v1/login
+exports.loginUser = catchAsyncError( async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) return next(new ErrorHandler('Please enter email & password', 400));
+
+   //Finding user in database 
+   const user = await User.findOne({ email }).select('+password');
+
+   if(!user) return next(new ErrorHandler('Invalid email or password'), 401)
+
+   const isPasswordMatched = await user.comparePassword(password);
+
+   if(!isPasswordMatched) return next(new ErrorHandler('Invalid email or password'))
+
+   res.status(200).send({
+        success: true,
+        user
+   });
+   
+});
 
 //currently login in user
 
