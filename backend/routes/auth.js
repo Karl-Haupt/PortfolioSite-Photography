b66d713f32cd4798c47aae0f7cfcdcb5729735b8
Karl-Haupt <karlhaupt1@gmail.com>
2021-11-11 19:01:19 +0200

    Connected the DB to the frontend || Created the login component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { loginUser, getUserProfile, logout } = require('../controllers/authController');
+const { registerUser, loginUser, getUserProfile, logout } = require('../controllers/authController');
 
-// router.route('/register').post(registerUser);
+router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/me').get(getUserProfile);
 router.route('/logout').get(logout);
---
+++ front-end/src/components/Login/Login.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import './login.css';
+
+import { login, logout, clearErrors } from '../../Redux/Actions/userActions';
+
+const Login = () => {
+    const alert = useAlert();
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [sidebar, setSidebar] = useState(false);
+
+    const { user, isAuthenticated, error, loading } = useSelector(state => state.auth);
+    
+    useEffect(() => {
+        if(isAuthenticated) {
+            console.log("Logined successfully");
+            alert.success(user.name)
+        }
+
+        if(loading && error) {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
+    }, [dispatch, alert, loading, error, isAuthenticated])
+
+    const submitHandler = e => {
+        e.preventDefault();
+        dispatch(login(email, password));
+    }
+
+    const showSidebar = () => setSidebar(!sidebar);
+    return (
+        <div className={sidebar ? "login active" : "login"} onClick={showSidebar}>
+            <form className="login__form" onSubmit={submitHandler}>
+                
+                        <div class="input--group">
+                            <input
+                                className="input"
+                                type="email"
+                                id="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                
+                        <div class="input--group">
+                            <input
+                                className="input"
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                <button class="login__btn" type="submit">Login</button>
+            </form>
+
+            {/* <button type="submit" onClick={logoutHandler}>Logout</button> */}
+        </div>
+    )
+}
+
+export default Login;
