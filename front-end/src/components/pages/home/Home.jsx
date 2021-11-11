e26a572384378cc325ff682eed77de29f9db23a0
Karl-Haupt <karlhaupt1@gmail.com>
2021-11-11 14:32:49 +0200

    Fixed React-Router-Dom

--- front-end/src/components/pages/home/Home.jsx
+++ front-end/src/components/pages/home/Home.jsx
@@ -1,9 +1,11 @@
-import "./home.scss";
+// import "./home.scss";
 
-export default function Home() {
+function Home() {
   return (
     <div className="home">
       <h1>Home</h1>
     </div>
   );
 }
+
+export default Home;
